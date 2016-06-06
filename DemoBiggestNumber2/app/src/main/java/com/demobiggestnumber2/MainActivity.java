afc7a25f5a94cacc8326949739152bda56e131b5
khangit96 <khangit69@gmail.com>
2016-06-06 14:07:55 +0700

    fskfdkdkdk

--- DemoBiggestNumber2/app/src/main/java/com/demobiggestnumber2/MainActivity.java
+++ DemoBiggestNumber2/app/src/main/java/com/demobiggestnumber2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.demobiggestnumber2;
 
 import android.app.Application;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,35 +14,138 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btContinue;
+    SharedPreferences sp;
+
+    /*Sound*/
+    private SoundPool soundPool;
+    private int soundIDPressButton, soundIDBackground;
+    boolean plays = false, loaded = false;
+    float actVolume, maxVolume, volume;
+    AudioManager audioManager;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
+        init();
+    }
+
+    public void init() {
+        initView();
+        initSound();
+        sp = getSharedPreferences("CHECK", Context.MODE_PRIVATE);
+        if (sp.getString("checkContinue", "").equals("ok")) {
+            btContinue.setVisibility(View.VISIBLE);
+        } else {
+            btContinue.setVisibility(View.GONE);
+        }
+        playBackgroundSound();
+    }
+
+    /*putDataSharedPreferences*/
+    public void putDataSharedPreferences(String name, String data) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString(name, data);
+        editor.commit();
+    }
+
+    private void initView() {
+        btContinue = (Button) findViewById(R.id.btContinue);
+        btContinue.setVisibility(View.GONE);
+
+    }
+
+    /*init sound*/
+    public void initSound() {
+        // AudioManager audio settings for adjusting the volume
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        actVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = actVolume / maxVolume;
+
+        //Hardware buttons setting to adjust the media sound
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+        // the counter will help us recognize the stream id of the sound played  now
+        counter = 0;
+
+        // Load the sounds
+        soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
+       /* soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
+            @Override
+            public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                loaded = true;
+            }
+        });*/
+        soundIDPressButton = soundPool.load(this, R.raw.press, 1);
+        soundIDBackground = soundPool.load(this, R.raw.intro, 1);
+
+    }
+
+    /*Play press button sound*/
+    public void playPressButtonSound() {
+        soundPool.play(soundIDPressButton, volume, volume, 1, 0, 1f);
+    }
+
+    /*Play background sound*/
+    public void playBackgroundSound() {
+        soundPool.play(soundIDBackground, volume, volume, 1, -1, 1f);
     }
 
     /*Event play game*/
-    public void Play(View v) {
-        startActivity(new Intent(MainActivity.this, PlayGameActivity.class));
+    public void newGame(View v) {
+        playPressButtonSound();
+        Intent newGameIntent = new Intent(MainActivity.this, PlayGameActivity.class);
+        newGameIntent.putExtra("game","new game");
+        startActivityForResult(newGameIntent, 0);
     }
 
-    /*High score*/
+    /*Continue game*/
+    public void Continue(View v) {
+        Intent newGameIntent = new Intent(MainActivity.this, PlayGameActivity.class);
+        newGameIntent.putExtra("game","continue");
+        startActivityForResult(newGameIntent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 0) {
+                btContinue.setVisibility(View.VISIBLE);
+                putDataSharedPreferences("checkContinue", "ok");
+            }
+        } else {
+            btContinue.setVisibility(View.GONE);
+            putDataSharedPreferences("checkContinue", "null");
+        }
+    }
+
+    /*  *//*High score*//*
     public void About(View v) {
         Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_LONG).show();
-    }
-
-    /*Instruction */
+    }*/
+/*
+    *//*Instruction *//*
     public void HowToPlay(View v) {
         Toast.makeText(getApplicationContext(), "How to play", Toast.LENGTH_LONG).show();
-    }
+    }*/
 
     /*Exit*/
     public void Exit(View v) {
+        playPressButtonSound();
         System.exit(0);
+
     }
 
-
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getApplicationContext(), "Please press Exit button to close game!", Toast.LENGTH_LONG).show();
+    }
 }
--- DemoBiggestNumber2/app/src/main/java/com/demobiggestnumber2/PlayGameActivity.java
+++ DemoBiggestNumber2/app/src/main/java/com/demobiggestnumber2/PlayGameActivity.java
@@ -1,15 +1,18 @@
 package com.demobiggestnumber2;
 
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.Message;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,40 +22,88 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import android.os.Handler;
-
-import android.util.Log;
-
+/**
+ * Created by Administrator on 6/5/2016.
+ */
 public class PlayGameActivity extends AppCompatActivity {
-    Button[] bt = new Button[16];
-    TextView tvScore, tvGameOver;
-    ImageView imgGameOver;
-    ArrayList<Integer> posRandomButtonArray = new ArrayList<>();
-    Random random = new Random();
+    float actVolume;
+    public AudioManager audioManager;
     private int biggestNumber = 0;
+    Button[] bt = new Button[16];
+    Button btContinue;
+    Button btPause;
+    Button btRestart;
+    private boolean checkGameOver = true;
     private int countQuestion = 0;
+    float maxVolume;
+    private int posBiggestNumber;
+    ArrayList<Integer> posRandomButtonArray = new ArrayList<>();
+    ProgressBar progressBar;
+    Random random = new Random();
     private int score = 0;
-    private int posBiggestNumber = 0;
+    private int soundIDGameOver;
+    private int soundIDPause;
+    private int soundIDPressButton;
+    private int soundIDSuccess;
+    private SoundPool soundPool;
+    SharedPreferences sp;
+    TextView tvGameOver;
+    TextView tvPause;
+    TextView tvScore;
+    TextView tvHightScore;
+    float volume;
     private Timer timer;
     private int count = 0;
-    ProgressBar progressBar;
-    private boolean checkGameOver = false;
+    int counter;
+    boolean plays = false, loaded = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_play_game);
-        addControls();
-        processRandomButton(2, 100);
+        init();
+    }
+
+    public void init() {
+        initView();
+        initSound();
+        sp = getSharedPreferences("CHECK", MODE_PRIVATE);
+        if (!sp.getString("score", "").equals("null")) {
+            String receivedData = getIntent().getStringExtra("game");
+            if (receivedData.equals("continue")) {
+                score = Integer.parseInt(sp.getString("score", ""));
+                countQuestion = Integer.parseInt(sp.getString("question", ""));
+                processRandomQuestion(countQuestion);
+                tvScore.setText("" + score);
+            } else if (receivedData.equals("new game")) {
+                score = 0;
+                tvScore.setText("" + score);
+                processRandomButton(2, 100);
+            }
+        } else {
+            processRandomButton(2, 100);
+        }
+        if (!sp.getString("highScore", "").equals("")) {
+            tvHightScore.setText(sp.getString("highScore", ""));
+        } else {
+            tvHightScore.setText(Integer.toString(0));
+        }
+
         startTimer(1000);
     }
 
-    public void addControls() {
+    public void initView() {
+        btRestart = (Button) findViewById(R.id.btRestart);
+        btPause = (Button) findViewById(R.id.btPause);
+        btContinue = (Button) findViewById(R.id.btContinue);
         tvScore = (TextView) findViewById(R.id.tvScore);
-    //    imgGameOver = (ImageView) findViewById(R.id.imgGameOver);
-        tvGameOver=(TextView)findViewById(R.id.tvGameOver);
+        tvPause = (TextView) findViewById(R.id.tvPause);
+        tvHightScore = (TextView) findViewById(R.id.tvHighScore);
+        tvGameOver = (TextView) findViewById(R.id.tvGameOver);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         tvGameOver.setVisibility(View.INVISIBLE);
+        btRestart.setVisibility(View.GONE);
+        btContinue.setVisibility(View.GONE);
+        tvPause.setVisibility(View.INVISIBLE);
         bt[0] = (Button) findViewById(R.id.bt0);
         bt[1] = (Button) findViewById(R.id.bt1);
         bt[2] = (Button) findViewById(R.id.bt2);
@@ -71,42 +122,81 @@ public class PlayGameActivity extends AppCompatActivity {
         bt[15] = (Button) findViewById(R.id.bt15);
     }
 
-    /*Button CLick event*/
+    public void initSound() {
+        // AudioManager audio settings for adjusting the volume
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        actVolume = (float) audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        maxVolume = (float) audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volume = actVolume / maxVolume;
+
+        //Hardware buttons setting to adjust the media sound
+        this.setVolumeControlStream(AudioManager.STREAM_MUSIC);
+        soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        soundIDSuccess = soundPool.load(this, R.raw.success, 1);
+        soundIDGameOver = soundPool.load(this, R.raw.gover, 1);
+        soundIDPause = soundPool.load(this, R.raw.pause, 1);
+        soundIDPressButton = soundPool.load(this, R.raw.press, 1);
+    }
+
+    public void putDataSharedPreferences(String name, String data) {
+        Editor editor = sp.edit();
+        editor.putString(name, data);
+        editor.commit();
+    }
+
     public void Choose(View v) {
-        String tag = (String) v.getTag();
-        if (Integer.parseInt(bt[Integer.parseInt(tag)].getText().toString()) == biggestNumber) {
+        if (Integer.parseInt(bt[Integer.parseInt((String) v.getTag())].getText().toString()) == biggestNumber) {
+            playSuccessSound();
             countQuestion++;
             score += 5;
-            progressBar.setProgress(100);
+            processRandomQuestion(countQuestion);
             tvScore.setText(Integer.toString(score));
-            if (countQuestion <= 3) {
-                processRandomButton(3, 100);
-            } else if (countQuestion >= 4 && countQuestion <= 7) {
-                processRandomButton(4, 100);
-            } else if (countQuestion >= 8 && countQuestion <= 11) {
-                processRandomButton(5, 100);
-            } else if (countQuestion >= 12 && countQuestion <= 14) {
-                processRandomButton(6, 100);
-            } else if (countQuestion >= 15 && countQuestion <= 18) {
-                processRandomButton(7, 100);
-            } else if (countQuestion >= 19 && countQuestion <= 21) {
-                processRandomButton(8, 100);
-            } else if (countQuestion >= 22 && countQuestion <= 23) {
-                processRandomButton(9, 100);
-            } else if (countQuestion >= 24 && countQuestion <= 26) {
-                processRandomButton(10, 100);
-            } else {
-                processRandomButton(16, 100);
-            }
             stopTimer();
             startTimer(1000);
-
         } else {
+            checkGameOver = false;
             gameOver();
+        }
+
+    }
+
+    public void processRandomQuestion(int question) {
+        if (question <= 3) {
+            processRandomButton(3, 100);
+        } else if (question >= 4 && question <= 7) {
+            processRandomButton(4, 100);
+        } else if (question >= 8 && question <= 11) {
+            processRandomButton(5, 100);
+        } else if (question >= 12 && question <= 14) {
+            processRandomButton(6, 100);
+        } else if (question >= 15 && question <= 18) {
+            processRandomButton(7, 100);
+        } else if (question >= 19 && question <= 21) {
+            processRandomButton(8, 100);
+        } else if (question >= 22 && question <= 23) {
+            processRandomButton(9, 100);
+        } else if (question < 24 || question > 26) {
+            processRandomButton(16, 100);
+        } else {
+            processRandomButton(10, 100);
         }
     }
 
-    /*Procces random button*/
+    public void playSuccessSound() {
+        soundPool.play(soundIDSuccess, volume, volume, 1, 0, 1.0f);
+    }
+
+    public void playGameOverSound() {
+        soundPool.play(soundIDGameOver, volume, volume, 1, 0, 1f);
+    }
+    public void playPauseSound() {
+        soundPool.play(soundIDPause, volume, volume, 1, 0, 1.0f);
+    }
+
+    public void playPressButtonSound() {
+        soundPool.play(soundIDPressButton, volume, volume, 1, 0, 1.0f);
+    }
+
     public void processRandomButton(int pos, int range) {
         goNextQuestion();
         for (int i = 0; i < pos; i++) {
@@ -114,7 +204,6 @@ public class PlayGameActivity extends AppCompatActivity {
             while (posRandomButtonArray.contains(r)) {
                 r = random.nextInt(16);
             }
-
             posRandomButtonArray.add(r);
         }
         for (int i = 0; i < posRandomButtonArray.size(); i++) {
@@ -133,21 +222,17 @@ public class PlayGameActivity extends AppCompatActivity {
         }
     }
 
-    /*Reset data game*/
     public void goNextQuestion() {
         biggestNumber = 0;
         posRandomButtonArray.clear();
         defaultData();
     }
 
-    /*reset data*/
     public void defaultData() {
         clearText();
         disableButton();
-
     }
 
-    /*Clear text*/
     public void clearText() {
         bt[0].setText("");
         bt[1].setText("");
@@ -167,7 +252,6 @@ public class PlayGameActivity extends AppCompatActivity {
         bt[15].setText("");
     }
 
-    /*Disable button*/
     public void disableButton() {
         bt[0].setEnabled(false);
         bt[1].setEnabled(false);
@@ -187,9 +271,19 @@ public class PlayGameActivity extends AppCompatActivity {
         bt[15].setEnabled(false);
     }
 
-
-    /*game over*/
     public void gameOver() {
+        if (!sp.getString("highScore", "").equals("")) {
+            int highScore = Integer.parseInt(sp.getString("highScore", ""));
+            if (score > highScore) {
+                putDataSharedPreferences("highScore", Integer.toString(score));
+                tvHightScore.setText(Integer.toString(score));
+            }
+        } else {
+            putDataSharedPreferences("highScore", Integer.toString(score));
+            tvHightScore.setText(Integer.toString(score));
+        }
+
+
         stopTimer();
         score = 0;
         bt[posBiggestNumber].setBackgroundResource(R.drawable.button_biggest_number);
@@ -198,20 +292,62 @@ public class PlayGameActivity extends AppCompatActivity {
         tvGameOver.setVisibility(View.VISIBLE);
         countQuestion = 0;
         biggestNumber = 0;
+        btRestart.setVisibility(View.VISIBLE);
+        btPause.setVisibility(View.GONE);
+        btContinue.setVisibility(View.GONE);
+        playGameOverSound();
     }
 
-    /*Restart game*/
     public void Restart(View v) {
-        bt[posBiggestNumber].setBackgroundResource(R.drawable.button_selector);
-        posBiggestNumber = 0;
-        tvScore.setText("");
+        this.checkGameOver = true;
+        this.btRestart.setVisibility(View.GONE);
+        this.btPause.setVisibility(View.VISIBLE);
+        this.bt[this.posBiggestNumber].setBackgroundResource(R.drawable.button_selector);
+        this.tvScore.setText("0");
+        this.posBiggestNumber = 0;
         clearText();
         disableButton();
         processRandomButton(2, 100);
-        tvGameOver.setVisibility(View.INVISIBLE);
+        this.tvGameOver.setVisibility(View.INVISIBLE);
         startTimer(1000);
+        playPressButtonSound();
     }
 
+    public void Pause(View v) {
+        clearText();
+        disableButton();
+        this.tvPause.setVisibility(View.VISIBLE);
+        this.btContinue.setVisibility(View.VISIBLE);
+        this.btPause.setVisibility(View.GONE);
+        stopTimer();
+        playPauseSound();
+    }
+
+    public void Continue(View v) {
+        this.btPause.setVisibility(View.VISIBLE);
+        this.btContinue.setVisibility(View.GONE);
+        this.tvPause.setVisibility(View.INVISIBLE);
+        continueTimer(1000);
+        processRandomQuestion(this.countQuestion);
+        playPressButtonSound();
+    }
+
+    private void continueTimer(int speed) {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                count = count + 10;
+                progressBar.setProgress(100 - (count * 2));
+                if (count == 50) {
+                    progressBar.setProgress(0);
+                    mHandler.obtainMessage(1).sendToTarget();
+
+                }
+
+            }
+        }, 1, speed);//speed coundown time
+    }
 
     /*Start timer*/
     public void startTimer(int Speed) {
@@ -221,8 +357,7 @@ public class PlayGameActivity extends AppCompatActivity {
             @Override
             public void run() {
                 count = count + 10;
-                progressBar.setProgress(100-(count*2));;
-                Log.d("test", "" + count);
+                progressBar.setProgress(100 - (count * 2));
                 if (count == 50) {
                     progressBar.setProgress(0);
                     mHandler.obtainMessage(1).sendToTarget();
@@ -245,5 +380,20 @@ public class PlayGameActivity extends AppCompatActivity {
     public void stopTimer() {
         timer.cancel();
     }
+
+
+    public void onBackPressed() {
+        stopTimer();
+        if (this.checkGameOver) {
+            putDataSharedPreferences("score", Integer.toString(this.score));
+            putDataSharedPreferences("question", Integer.toString(countQuestion));
+            setResult(RESULT_OK, new Intent());
+        } else {
+            putDataSharedPreferences("score", "null");
+        }
+        finish();
+        super.onBackPressed();
+    }
+
 
 }
